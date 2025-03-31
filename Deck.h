3cbacec8f907ca62f0a32a81134fbe563f40e329
beejdrik <brntfader@gmail.com>
2025-03-31 19:41:19 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,7 +1,3 @@
-//
-// Created by Owner on 3/31/2025.
-//
-
 #ifndef DECK_H
 #define DECK_H
 #include <iostream>
@@ -21,19 +17,28 @@ class Card {
     string getRank() const { return rank; }
     void setSuit(string s) { suit = s; }
     void setRank(string r) { rank = r; }
-
-    void Print() const {
-        cout << getSuit() << " " << getRank() << endl;
-    }
-
-
 };
+
 class Deck {
-int clubs[13]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-int spades[13]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-int hearts[13]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-int diamonds[13]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+private:
+    Card deck[52]{};
+
+    string suit[4] = {"Clubs", "Spades", "Diamonds", "Hearts"};
+    string rank[13] = {"Ace", "Two", "Three", "Four",
+        "Five", "Six", "Seven", "Eight", "Nine", "Ten"
+        ,"Jack", "Queen", "King"};
+    public:
+    Deck() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                deck[i * 13 + j].setSuit(suit[i]);
+                deck[i * 13 + j].setRank(rank[j]);
+            }
+        }
+    }
 };
+
+
 
 
 
