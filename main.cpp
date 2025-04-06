09cbf3b6b4ebd98f9460912b98ff881f8d41acd9
beejdrik <brntfader@gmail.com>
2025-04-05 20:05:08 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Deck.h"
+int main() {
+    Deck deck = Deck();
+    deck.dealCard().Print();
+    deck.dealCard().Print();
+    deck.dealCard().Print();
+    deck.dealCard().Print();
+    deck.dealCard().Print();
+
+
+    return 0;
+}
