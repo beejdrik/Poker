dd4e7c63bebf0152f1cb609ee7f5b2b02f26f83e
beejdrik <brntfader@gmail.com>
2025-04-05 20:04:41 -0400

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,9 +1,14 @@
 #ifndef DECK_H
 #define DECK_H
+#include <algorithm>
 #include <iostream>
 #include <ostream>
+#include <vector>
 using namespace std;
 #include <string>
+#include <list>
+#include <ctime>
+#include <cstdlib>
 
 class Card {
     private:
@@ -17,11 +22,14 @@ class Card {
     string getRank() const { return rank; }
     void setSuit(string s) { suit = s; }
     void setRank(string r) { rank = r; }
+    void Print() {
+        cout << rank << " " << suit << endl;
+    }
 };
 
 class Deck {
 private:
-    Card deck[52]{};
+    list<Card> deck;
 
     string suit[4] = {"Clubs", "Spades", "Diamonds", "Hearts"};
     string rank[13] = {"Ace", "Two", "Three", "Four",
@@ -31,10 +39,20 @@ private:
     Deck() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 13; j++) {
-                deck[i * 13 + j].setSuit(suit[i]);
-                deck[i * 13 + j].setRank(rank[j]);
+                deck.push_back(Card(suit[i], rank[j]));
             }
         }
+        srand(time(nullptr));
+        vector<Card> temp(deck.begin(), deck.end());
+        random_shuffle(temp.begin(), temp.end());
+        copy(temp.begin(), temp.end(), deck.begin());
+    }
+    Card dealCard() {
+
+        Card newCard = deck.front();
+        deck.pop_front();
+      return newCard;
+
     }
 };
 
